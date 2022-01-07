d7beb4acf8ebe7da8f973593252222205f461020
DaniilPavlyuchkov <kattomas@yandex.ru>
2022-01-07 16:29:49 +0300

    Выполнено 2-2

---
+++ Task 2-2/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+/**
+*\brief Функция для рассчета y1
+*\param x число, вводимое пользователем
+*\return Возвращает значение y1
+**/
+double getFirstY(const double a, double x);
+
+/**
+*\brief Функция для рассчета y2
+*\param x число, вводимое пользователем
+*\return Возвращает значение y2
+**/
+double getSecondY(const double a, double x);
+
+/**
+*\brief Функция для рассчета y3
+*\param x число, вводимое пользователем
+*\return Возвращает значение y3
+**/
+double getThirdY(const double a, double x);
+
+int main()
+{
+	const double a = 2.5;
+
+	cout << "Введите x = ";
+	double x;
+	cin >> x;
+
+	if (x < 0.5)
+	{
+		double firstY = getFirstY(a, x);
+		cout << firstY;
+	}
+	else if (x == 0.5)
+	{
+		double secondY = getSecondY(a, x);
+		cout << secondY;
+	}
+	else
+	{
+		double thirdY = getThirdY(a, x);
+		cout << thirdY;
+	}
+
+	return 0;
+}
+
+double getFirstY(const double a, double x)
+{
+	return sqrt(a * x * x + 1);
+}
+
+double getSecondY(const double a, double x)
+{
+	return a * x + 3;
+}
+
+double getThirdY(const double a, double x)
+{
+	return sqrt(a * a * x - 1);
+}
